759d0a28f60847aacefa60367da283bf1939155d
felix-hh <50616313+felix-hh@users.noreply.github.com>
2020-09-21 20:27:36 -0500

    edit courses

---
+++ components/CourseSelector.js
@@ -0,0 +1,37 @@
+import React, {useContext, useState, useEffect } from 'react';
+import { SafeAreaView, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import Course from "./Course"
+import { getCourseNumber, getCourseTerm, hasConflict, terms } from "../utils/course"
+
+const CourseSelector = ({courses, view}) => {
+  const [selected, setSelected] = useState([]);
+
+  const toggle = course => setSelected(selected => (
+    selected.includes(course) ? selected.filter(x => x !== course) : [...selected, course]
+  ));
+  return (
+    <View style={styles.courseList}>
+      {
+        courses.map(course => (
+          <Course key={course.id} course={course} view={view}
+            isDisabled={hasConflict(course, selected)}
+            isSelected={selected.includes(course)}
+            select={toggle}
+          />
+        ))
+      }
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+    courseList: {
+        flex: 1,
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+      }
+})
+
+export default CourseSelector
